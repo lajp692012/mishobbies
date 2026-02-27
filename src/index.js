533a9af6fc71025539bc55021d294a06fa7d3dd0
lajp692012 <lajp69@gmail.com>
2026-02-26 19:04:06 -0500

    Actualizacion puerto

--- src/index.js
+++ src/index.js
@@ -50,6 +50,6 @@ app.get('/data', (req, res) => {
   }
 });
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 
-console.log('Servidor está escuchando', 3000)
+console.log('Servidor está escuchando', process.env.PORT || 3000)
